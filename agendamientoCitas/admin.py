6b17283e9158d7e32fe400ed15f3dafe4f4b22c7
SebastianCely <juanescely@hotmail.com>
2026-05-16 18:13:53 -0500

    modelos

--- agendamientoCitas/admin.py
+++ agendamientoCitas/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Doctor
+from .models import TipoCita
+from .models import Paciente
+from .models import Cita
+
+admin.site.register(Doctor)
+admin.site.register(TipoCita)
+admin.site.register(Paciente)
+admin.site.register(Cita)
--- agendamientoCitas/models.py
+++ agendamientoCitas/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class TipoCita(models.Model):
+    nombre = models.CharField(max_length=200)
+
+class Doctor(models.Model):
+    primer_apellido = models.CharField(max_length=15)
+    segundo_apellido = models.CharField(max_length=15, null=True)
+    primer_nombre = models.CharField(max_length=15)
+    segundo_nombre = models.CharField(max_length=15, null=True)
+    tipos_cita = models.ManyToManyField(TipoCita, related_name='doctores')
+
+
+class Paciente(models.Model):
+    primer_apellido = models.CharField(max_length=15)
+    segundo_apellido = models.CharField(max_length=15, null=True)
+    primer_nombre = models.CharField(max_length=15)
+    segundo_nombre = models.CharField(max_length=15, null=True)
+    numero_identificacion = models.PositiveIntegerField()
+
+
+class Cita(models.Model):
+    paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE)
+    tipo_cita = models.ForeignKey(TipoCita, on_delete=models.CASCADE)
+    fecha = models.DateField()
